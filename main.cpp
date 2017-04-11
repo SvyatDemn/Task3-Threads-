ee17568e40e5f6f7938c5027ed836f93505f06fa
Svyatoslav Demnyak <cjiabi1994@gmail.com>
2017-04-11 21:48:02 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,9 +12,9 @@ int main()
 	std::vector<std::thread> parsing;
 	std::thread searching(std::bind(&Data::Searching, std::ref(data), root_folder));
 	searching.join();
-	for(unsigned int i = 0; i < threads - 1; i++)
+	for(unsigned int i = 0; i < threads; i++)
 		parsing.emplace_back(std::thread(std::bind(&Data::Parsing, std::ref(data))));
-	for(unsigned int j = 0; j < parsing.size(); ++j)
+	for(unsigned int j = 0; j < parsing.size(); j++)
 		parsing[j].join();
 	std::chrono::high_resolution_clock::time_point finish = std::chrono::high_resolution_clock::now();
 	auto finaltime = std::chrono::duration_cast<std::chrono::seconds>(finish-start).count();
